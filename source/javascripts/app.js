7a82e564a5962fc8eacaad61d8d171475007a9ad
samwong <wongsamuel@gmail.com>
2017-06-13 23:08:36 -0700

    update

--- source/javascripts/app.js
+++ source/javascripts/app.js
@@ -1,7 +1,29 @@
 angular
-  .module( 'starter-app', [ 'ngMaterial' ] )
+  .module( 'starter-app', [ 'angular-d3-word-cloud', 'ngMaterial' ] )
   .controller('AppCtrl', function($scope) {
-  $scope.imagePath = 'img/washedout.png';
+    //$scope.imagePath = 'img/washedout.png';
+    $scope.width=200;
+    $scope.height=200;
+    $scope.words=[
+      {text: 'Performance',size: 25},
+      {text: 'BigData',size: 20},
+      {text: 'Scalability',size: 25},
+      {text: 'Bottleneck',size: 20},
+      {text: 'SysStats',size: 20},
+      {text: 'nmon',size: 20},
+      {text: 'Hadoop',size: 20},
+      {text: 'ETL',size: 20},
+      {text: 'DataStage',size: 20},
+      {text: 'SQL',size: 15},
+      {text: 'Kafka',size: 15},
+      {text: 'Zookeeper',size: 15},
+      {text: 'Spark',size: 15},
+      {text: 'Scala',size: 15},
+      {text: 'Java',size: 20},
+      {text: 'JVM',size: 20},
+      {text: 'C/C++',size: 10}
+   ];
+
 })
 .config(function($mdThemingProvider) {
   $mdThemingProvider.theme('dark-grey').backgroundPalette('grey').dark();
